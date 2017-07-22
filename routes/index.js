35c4a34ab43aa0a34186f34670d4aae2bf77a088
Adrian Gonzalo <gonzahh@gmail.com>
2017-07-22 19:27:15 +0200

    work in progress

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const achievementController = require('../controllers/achievementController')
+const userController = require('../controllers/userController')
 const { catchErrors } = require('../handlers/errorHandlers')
 
 // Route handling time!
@@ -20,5 +21,34 @@ router.post('/achievements/:id',
   catchErrors(achievementController.resize),
   catchErrors(achievementController.updateAchievement)
 )
+
+// Users stuff:
+/*
+router.get('/account')
+router.post('/account')
+router.post('/account/forgot')
+router.get('/account/reset/:token')
+router.post('/account/reset/:token')
+router.get('/register')
+router.post('/register')
+router.get('/login')
+router.post('/login')
+router.get('/logout')
+*/
+
+// Organizations stuff:
+/*
+router.get('/organizations')
+router.get('/organizations/add')
+router.post('/organizations/add')
+router.get('/organizations/:slug')
+router.post('/organizations/:id')
+*/
+
+// Groups stuff:
+/*
+manage groups creation inside organization page?
+*/
+
 
 module.exports = router
